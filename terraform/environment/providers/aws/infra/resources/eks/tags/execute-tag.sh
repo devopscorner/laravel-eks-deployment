26a75e33d82698455d8ca0679bce8cbf4a7df1a9
Dwi Fahni Denni <dfdenni0912@gmail.com>
2022-11-20 10:45:00 +0700

    Added taggination fixing script

---
+++ terraform/environment/providers/aws/infra/resources/eks/tags/execute-tag.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python3 tag-devops-monitoring.py
+python3 tag-devops-tools.py
+python3 tag-sharedredis-dev.py
+python3 tag-sharedredis-uat.py
+python3 tag-spinnaker-devops.py
---
+++ terraform/environment/providers/aws/infra/resources/eks/tags/tag-sharedredis.py
@@ -0,0 +1,145 @@
+import boto3
+
+asg_client = boto3.client('autoscaling')
+ec2_client = boto3.client('ec2')
+
+## Add ASG name value and nodegroup name here
+asg_value = 'eks-1234567890'
+eks_nodegroup = 'devopscorner-sharedredis_node'
+
+env = 'DEV'
+types = 'PRODUCTS'
+productname = 'DEVOPSCORNER-SHAREDREDIS'
+dept = 'SOFTENG'
+service = 'REDIS'
+clustername = 'devopscorner-staging'
+
+response = asg_client.create_or_update_tags(
+    Tags=[
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'Name',
+            'Value': 'EKS-1.22-'+eks_nodegroup.upper(),
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'Department',
+            'Value': dept,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'DepartmentGroup',
+            'Value': env+'-'+dept,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'Environment',
+            'Value': env,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'ProductName',
+            'Value': productname,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'ProductGroup',
+            'Value': env+'-'+productname,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'Type',
+            'Value': types,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'ResourceGroup',
+            'Value': env+'-EKS-'+productname,
+            'PropagateAtLaunch': True
+        },
+        {
+            'ResourceId': asg_value,
+            'ResourceType': 'auto-scaling-group',
+            'Key': 'Service',
+            'Value': service,
+            'PropagateAtLaunch': True
+        }
+    ]
+)
+
+responses = ec2_client.describe_instances(
+        Filters=[
+            {
+            'Name':'tag:eks:nodegroup-name',
+            'Values': [eks_nodegroup]
+            }
+        ]
+    )
+
+instances_list = []
+for reservation in responses["Reservations"]:
+    for instance in reservation["Instances"]:
+        instances_list.append(instance["InstanceId"])
+
+for i in instances_list:
+    print(i)
+    response = ec2_client.create_tags(
+        Resources=[i],
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'EKS-1.22-'+eks_nodegroup.upper()
+            },
+            {
+                'Key': 'Department',
+                'Value': dept
+            },
+            {
+                'Key': 'DepartmentGroup',
+                'Value': env+'-'+dept
+            },
+            {
+                'Key': 'Environment',
+                'Value': env
+            },
+            {
+                'Key': 'ProductName',
+                'Value': productname
+            },
+            {
+                'Key': 'ProductGroup',
+                'Value': env+'-'+productname
+            },
+            {
+                'Key': 'Type',
+                'Value': types
+            },
+            {
+                'Key': 'ResourceGroup',
+                'Value': env+'-EKS-'+productname
+            },
+            {
+                'Key': 'Service',
+                'Value': service
+            },
+            {
+                'Key': 'ClusterName',
+                'Value': clustername
+            }
+        ]
+    )
